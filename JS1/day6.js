61caebd9f1ef83cb1df942e4121cd5a4e5c31189
aradhya121 <97972087+aradhya121@users.noreply.github.com>
2024-02-07 12:44:46 +0530

    Add files via upload

---
+++ JS1/day6.js
@@ -0,0 +1,16 @@
+let book = {
+    title: "The Complete Novel of Sherlock Holmes",
+    author: "Arthur Conan Doyle",
+    year: "2017",
+    isAvailable: "yes "
+};
+console.log("accessing by dot notation")
+console.log(book.title)
+console.log(book.author)
+console.log(book.year)
+console.log(book.isAvailable)
+console.log("accessing by bracket notation")
+console.log(book["title"])
+console.log(book["author"])
+console.log(book["year"])
+console.log(book["isAvailable"])
